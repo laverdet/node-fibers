cadde662886a67fdcf08669a63a2bf4902b51cbb
Marcel Laverdet <marcel@laverdet.com>
2012-01-04 20:30:58 -0600

    Fix unit test script from gobbling up errors

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -e
 for ii in `ls test`
 	do echo -n $ii': '
 	OUTPUT=$(NODE_PATH=`pwd` node test/$ii 2>&1)
-	echo $OUTPUT
+	echo "$OUTPUT"
 	if [[ "$OUTPUT" != "pass" ]]; then
 		exit 1
 	fi
