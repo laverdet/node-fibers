71cc7b2029b3beea784aa8c7b050ae5f488044ce
Marcel Laverdet <marcel@laverdet.com>
2012-02-03 00:46:04 -0600

    Build on arm
    This maybe fixes gh-48. I don't have an arm machine to test on, but I
    added the changes suggested in the ticket by @apsystems.

--- src/platform.mk
+++ src/platform.mk
@@ -1,31 +1,39 @@
 # I know nothing about scons, waf, or autoconf. Sorry.
-NODE_PREFIX := $(shell echo "console.log(require('path').dirname(require('path').dirname(process.execPath)))" | node)
-NODE_PLATFORM := $(shell echo "console.log(process.platform.replace('2', ''))" | node)
-NODE_BITS := $(shell file `echo "console.log(process.execPath)" | node` | egrep -o '[0-9]{2}-bit' | cut -c-2)
+NODE_PREFIX := $(shell node -e "console.log(require('path').dirname(require('path').dirname(process.execPath)))")
+NODE_PLATFORM := $(shell node -e "console.log(process.platform.replace('2', ''))")
+NODE_BITS := $(shell file `node -e "console.log(process.execPath)"` | egrep -o '[0-9]{2}-bit' | cut -c-2)
 
 CPPFLAGS = -Wall -Wno-deprecated-declarations -I$(NODE_PREFIX)/include -I$(NODE_PREFIX)/include/node
 ifdef DEBUG
 	CPPFLAGS += -ggdb -O0
 else
-	CPPFLAGS += -g -O3 -minline-all-stringops
+	CPPFLAGS += -g -O3
 endif
 
-ifeq ($(NODE_BITS), )
-	CPPFLAGS += -m32
-endif
-ifeq ($(NODE_BITS), 32)
-	CPPFLAGS += -m32
-endif
-ifeq ($(NODE_BITS), 64)
-	CPPFLAGS += -m64
+ifneq ($(HOSTTYPE), arm)
+	ifeq ($(NODE_BITS), )
+		CPPFLAGS += -m32
+	endif
+	ifeq ($(NODE_BITS), 32)
+		CPPFLAGS += -m32
+	endif
+	ifeq ($(NODE_BITS), 64)
+		CPPFLAGS += -m64
+	endif
 endif
 
 ifeq ($(NODE_PLATFORM), linux)
-	# SJLJ in linux = hangs & segfaults
-	CPPFLAGS += -DCORO_UCONTEXT
+	ifeq ($(HOSTTYPE), arm)
+		# SJLJ & UCONTEXT don't work on arm (?)
+		CPPFLAGS += -DCORO_PTHREAD
+	else
+		# SJLJ in linux = hangs & segfaults
+		CPPFLAGS += -DCORO_UCONTEXT
+	endif
 endif
 ifeq ($(NODE_PLATFORM), sunos)
-       CPPFLAGS += -DCORO_UCONTEXT
+	# Same as Linux
+	CPPFLAGS += -DCORO_UCONTEXT
 endif
 ifeq ($(NODE_PLATFORM), darwin)
 	# UCONTEXT in os x = hangs & segfaults :(
