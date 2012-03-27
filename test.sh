f64b36f309798fc7be9dec8044230a1f68bd274d
Marcel Laverdet <marcel@laverdet.com>
2012-03-26 21:33:49 -0500

    Don't use [[ .. ]] in test.sh
    sh needs [ .. ]
    
    Fixes gh-60

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ for ii in `ls test`; do
 	$(which echo) -n $ii': '
 	OUTPUT=$(NODE_PATH=`pwd` node test/$ii 2>&1)
 	echo "$OUTPUT"
-	if [[ "$OUTPUT" != "pass" ]]; then
+	if [ "$OUTPUT" != "pass" ]; then
 		exit 1
 	fi
 done
