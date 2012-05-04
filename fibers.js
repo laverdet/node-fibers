46595cc5b8718c910001cf955042afa8c6552391
Marcel Laverdet <marcel@laverdet.com>
2012-05-04 17:42:20 -0500

    Remove mtime check on fibers require()
    If there are problems with the build, they aren't going to be from
    upgrading Node. Plus I want to move to binary distribution soon if
    possible and this will be counterproductive.

--- fibers.js
+++ fibers.js
@@ -1,13 +1,4 @@
 var fs = require('fs'), path = require('path');
 
-if (fs.statSync(process.execPath).mtime >
-		fs.statSync(require.resolve('./src/fibers')).mtime) {
-	throw new Error(
-		'`node` has a newer mtime than `fiber`; it is possible your build is out of date. This ' +
-		'could happen if you upgrade node. Try `npm rebuild fibers` to rebuild. If that doesn\'t ' +
-		'work you could consider running `touch ' + __dirname + 'src/fibers` and maybe there won\'t ' +
-		'be a problem.');
-}
-
 // Injects `Fiber` and `yield` in to global
 require('./src/fibers');
