e2cb28cf5f39a50090e0bae4c7566b668f174082
Marcel Laverdet <marcel@laverdet.com>
2012-05-09 03:57:20 -0500

    Pure-JS test script
    This removes the last dependency on bash.
    
    Also fix a couple of issues with the npm package file.

---
+++ test.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+var path = require('path');
+
+function runTest(test, cb) {
+	var proc = spawn(process.execPath, [path.join('test', test)], {env: {NODE_PATH: __dirname}});
+	proc.stdout.setEncoding('utf8');
+	proc.stderr.setEncoding('utf8');
+
+	var stdout = '', stderr = '';
+	proc.stdout.on('data', function(data) {
+		stdout += data;
+	});
+	proc.stderr.on('data', function(data) {
+		stderr += data;
+	});
+	proc.stdin.end();
+
+	proc.on('exit', function(code) {
+		console.log(test+ ': '+ 'pass');
+		if (stdout !== 'pass\n' || stderr !== '') {
+			return cb(new Error(
+					'Test `'+ test+ '` failed.\n'+
+					'code: '+ code+ '\n'+
+					'stderr: '+ stderr+ '\n'+
+					'stdout: '+ stdout));
+		}
+		cb();
+	});
+}
+
+var cb = function(err) {
+	if (err) {
+		console.error(String(err));
+		process.exit(1);
+	}
+};
+fs.readdirSync('./test').reverse().forEach(function(file) {
+	cb = new function(cb, file) {
+		return function(err) {
+			if (err) return cb(err);
+			runTest(file, cb);
+		};
+	}(cb, file);
+});
+cb();
--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-for ii in `ls test`; do
-	$(which echo) -n $ii': '
-	OUTPUT=$(NODE_PATH=`pwd` node test/$ii 2>&1)
-	echo "$OUTPUT"
-	if [ "$OUTPUT" != "pass" ]; then
-		exit 1
-	fi
-done
