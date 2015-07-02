laverdet/node-fibers

commit bdea53350c454bd50326a52fddd28d15be7830ac

Fix segfault on exit when unwound fibers exist
Newer versions of v8 raise an exception when you try to delete an
isolate while other threads still have an active entry. This causes node
to crash instead of exiting gracefully. It's not too big of a deal since
we were in the process of terminating and no more user code could run
anyway, but it is still confusing.

Also this updates the unit tests to detect unusual exit codes and report
them as error. And also to exit with an error code when a test fails.

Fixes #223

src/fibers.cc
@@ -553,7 +553,9 @@ class Fiber {
 			// already running.
 			{
 				Unlocker unlocker(isolate);
+				isolate->Exit();
 				this_fiber->run();
+				isolate->Enter();
 			}
 
 			// At this point the fiber either returned or called `yield()`.
@@ -673,7 +675,9 @@ class Fiber {
 			{
 				Unlocker unlocker(that.isolate);
 				that.yielding = true;
+				that.isolate->Exit();
 				that.entry_fiber->run();
+				that.isolate->Enter();
 				that.yielding = false;
 			}
 			// Now `run()` has been called again.

test.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var spawn = require('child_process').spawn;
 var path = require('path');
 
+var ret = 0;
 function runTest(test, cb) {
 	var env = {};
 	for (var ii in process.env) {
@@ -28,12 +29,16 @@ function runTest(test, cb) {
 
 	proc.on('exit', function(code) {
 		if (stdout !== 'pass\n' || stderr !== '') {
+			ret = 1;
 			console.error(
 				test+ ': *fail*\n'+
 				'code: '+ code+ '\n'+
 				'stderr: '+ stderr+ '\n'+
 				'stdout: '+ stdout
 			);
+		} else if (code !== 0) {
+			ret = 1;
+			console.error(test+ ': fail ('+ code+ ')');
 		} else {
 			console.log(test+ ': '+ 'pass');
 		}
@@ -41,18 +46,15 @@ function runTest(test, cb) {
 	});
 }
 
-var cb = function(err) {
-	if (err) {
-		console.error(String(err));
-		process.exit(1);
-	}
+var cb = function() {
+	process.exit(ret);
 };
 fs.readdirSync('./test').reverse().forEach(function(file) {
-	cb = new function(cb, file) {
+	cb = new function(cb) {
 		return function(err) {
 			if (err) return cb(err);
 			runTest(file, cb);
 		};
-	}(cb, file);
+	}(cb);
 });
 cb();

commit dc32b477c4b5f6fe2c59657ef56389f36c773774

Fix node 0.10.x
This was broken in bdea5335 because I guess isolate semantics changed

src/fibers.cc
@@ -131,6 +131,17 @@ namespace uni {
 		return Signature::New(isolate, receiver);
 	}
 
+	class ReverseIsolateScope {
+		Isolate* isolate;
+		public:
+			explicit inline ReverseIsolateScope(Isolate* isolate) : isolate(isolate) {
+				isolate->Exit();
+			}
+			inline ~ReverseIsolateScope() {
+				isolate->Enter();
+			}
+	};
+
 	void AdjustAmountOfExternalAllocatedMemory(Isolate* isolate, int64_t change_in_bytes) {
 		isolate->AdjustAmountOfExternalAllocatedMemory(change_in_bytes);
 	}
@@ -249,6 +260,10 @@ namespace uni {
 	) {
 		return Signature::New(receiver, argc, argv);
 	}
+
+	class ReverseIsolateScope {
+		public: explicit inline ReverseIsolateScope(Isolate* isolate) {}
+	};
 
 	void AdjustAmountOfExternalAllocatedMemory(Isolate* isolate, int64_t change_in_bytes) {
 		V8::AdjustAmountOfExternalAllocatedMemory(change_in_bytes);
@@ -552,9 +567,8 @@ class Fiber {
 			// already running.
 			{
 				Unlocker unlocker(isolate);
-				isolate->Exit();
+				uni::ReverseIsolateScope isolate_scope(isolate);
 				this_fiber->run();
-				isolate->Enter();
 			}
 
 			// At this point the fiber either returned or called `yield()`.
@@ -673,10 +687,9 @@ class Fiber {
 			// keep the handle around.
 			{
 				Unlocker unlocker(that.isolate);
+				uni::ReverseIsolateScope isolate_scope(that.isolate);
 				that.yielding = true;
-				that.isolate->Exit();
 				that.entry_fiber->run();
-				that.isolate->Enter();
 				that.yielding = false;
 			}
 			// Now `run()` has been called again.