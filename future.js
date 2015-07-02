562f02acccd1931935bebf2aa057b64a072f594d
Marcel Laverdet <marcel@laverdet.com>
2015-07-02 16:17:52 -0500

    Throw original Errors in Future
    Instead of throwing a derived Error in Future.prototype.get we now throw
    the original error but with a mutated `stack` property. This could run
    into problems if you're catching the same error from multiple places at
    the same time, but even then it shouldn't cause *too* much confusion.
    
    Fixes #206

--- future.js
+++ future.js
@@ -193,26 +193,35 @@ Future.prototype = {
 			throw new Error('Future must resolve before value is ready');
 		} else if (this.error) {
 			// Link the stack traces up
-			var stack = {}, error = this.error instanceof Object ? this.error : new Error(this.error);
-			var longError = Object.create(error);
-			Error.captureStackTrace(stack, Future.prototype.get);
-			Object.defineProperty(longError, 'stack', {
-				get: function() {
-					var baseStack = error.stack;
-					if (baseStack) {
-						baseStack = baseStack.split('\n');
-						return [baseStack[0]]
-							.concat(stack.stack.split('\n').slice(1))
-							.concat('    - - - - -')
-							.concat(baseStack.slice(1))
-							.join('\n');
-					} else {
-						return stack.stack;
-					}
-				},
-				enumerable: true,
-			});
-			throw longError;
+			var error = this.error;
+			var localStack = {};
+			Error.captureStackTrace(localStack, Future.prototype.get);
+			var futureStack = Object.getOwnPropertyDescriptor(error, 'futureStack');
+			if (!futureStack) {
+				futureStack = Object.getOwnPropertyDescriptor(error, 'stack');
+				if (futureStack) {
+					Object.defineProperty(error, 'futureStack', futureStack);
+				}
+			}
+			if (futureStack && futureStack.get) {
+				Object.defineProperty(error, 'stack', {
+					get: function() {
+						var stack = futureStack.get.apply(error);
+						if (stack) {
+							stack = stack.split('\n');
+							return [stack[0]]
+								.concat(localStack.stack.split('\n').slice(1))
+								.concat('    - - - - -')
+								.concat(stack.slice(1))
+								.join('\n');
+						} else {
+							return localStack.stack;
+						}
+					},
+					enumerable: true,
+				});
+			}
+			throw error;
 		} else {
 			return this.value;
 		}
