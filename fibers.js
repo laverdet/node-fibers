584e8c324ce93b344fd0a7b8f52a38fba67a2a4b
Ashley <ForsakenHarmony@users.noreply.github.com>
2016-09-19 18:32:12 +0200

    Update fibers.js

--- fibers.js
+++ fibers.js
@@ -1,24 +1,25 @@
 if (process.fiberLib) {
-	return module.exports = process.fiberLib;
+	module.exports = process.fiberLib;
+} else {
+	var fs = require('fs'), path = require('path');
+
+	// Seed random numbers [gh-82]
+	Math.random();
+
+	// Look for binary for this platform
+	var modPath = path.join(__dirname, 'bin', process.platform+ '-'+ process.arch+ '-'+ process.versions.modules, 'fibers');
+	try {
+		fs.statSync(modPath+ '.node');
+	} catch (ex) {
+		// No binary!
+		console.error(
+			'## There is an issue with `node-fibers` ##\n'+
+			'`'+ modPath+ '.node` is missing.\n\n'+
+			'Try running this to fix the issue: '+ process.execPath+ ' '+ __dirname.replace(' ', '\\ ')+ '/build'
+		);
+		throw new Error('Missing binary. See message above.');
+	}
+
+	// Pull in fibers implementation
+	process.fiberLib = module.exports = require(modPath).Fiber;
 }
-var fs = require('fs'), path = require('path');
-
-// Seed random numbers [gh-82]
-Math.random();
-
-// Look for binary for this platform
-var modPath = path.join(__dirname, 'bin', process.platform+ '-'+ process.arch+ '-'+ process.versions.modules, 'fibers');
-try {
-	fs.statSync(modPath+ '.node');
-} catch (ex) {
-	// No binary!
-	console.error(
-		'## There is an issue with `node-fibers` ##\n'+
-		'`'+ modPath+ '.node` is missing.\n\n'+
-		'Try running this to fix the issue: '+ process.execPath+ ' '+ __dirname.replace(' ', '\\ ')+ '/build'
-	);
-	throw new Error('Missing binary. See message above.');
-}
-
-// Pull in fibers implementation
-process.fiberLib = module.exports = require(modPath).Fiber;
